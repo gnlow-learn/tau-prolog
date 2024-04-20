e0f44bd3572600ecb7ed9db76067c704b94059d2
Gnlow <gnlow@protonmail.com>
2024-04-20 11:35:43 +0900

    library(js)

--- mod.ts
+++ mod.ts
@@ -1,14 +1,23 @@
 import pl from "npm:tau-prolog@0.3.4"
+
+import useJs from "npm:tau-prolog@0.3.4/modules/js.js"
+useJs(pl)
+
 import usePromise from "npm:tau-prolog@0.3.4/modules/promises.js"
 usePromise(pl)
+
+globalThis.succ = (x: number) => {
+    console.log(x)
+    return x + 1
+}
 
 const session = pl.create()
 
 await session.promiseConsult(`
-    likes(a, b).
+    :- use_module(library(js)).
 `)
 await session.promiseQuery(`
-    likes(a, X).
+    apply(succ, [1], Value).
 `)
 for await (const answer of session.promiseAnswers()) {
     console.log(session.format_answer(answer))

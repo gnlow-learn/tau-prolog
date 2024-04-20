6e6f32cf88eedadb0638567033c0533d8eb10663
Gnlow <gnlow@protonmail.com>
2024-04-20 10:31:07 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,20 +1,15 @@
 import pl from "npm:tau-prolog@0.3.4"
+import usePromise from "npm:tau-prolog@0.3.4/modules/promises.js"
+usePromise(pl)
 
 const session = pl.create()
 
-session.consult(`
-    like(a, b).
+await session.promiseConsult(`
+    likes(a, b).
 `)
-session.query(
-    `
-        like(a, X).
-    `,
-    {
-        success(goal: unknown) {
-            console.log(goal)
-        },
-        error(err: unknown) {
-            console.log(err)
-        }
-    }
-)
+await session.promiseQuery(`
+    likes(a, X).
+`)
+for await (const answer of session.promiseAnswers()) {
+    console.log(session.format_answer(answer))
+}

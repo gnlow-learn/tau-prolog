be3f566c6196a14eba0583d21d930fe5f11262a6
Gnlow <gnlow@protonmail.com>
2024-04-20 11:51:43 +0900

    deSucc

--- mod.ts
+++ mod.ts
@@ -7,17 +7,25 @@ import usePromise from "npm:tau-prolog@0.3.4/modules/promises.js"
 usePromise(pl)
 
 globalThis.succ = (x: number) => {
-    console.log(x)
+    console.log("// succ", x)
     return x + 1
+}
+globalThis.deSucc = (x: number) => {
+    console.log("// deSucc", x)
+    return x - 1
 }
 
 const session = pl.create()
 
 await session.promiseConsult(`
     :- use_module(library(js)).
+    Y = succ(X) :-
+        apply(succ, [X], Y);
+        apply(deSucc, [Y], X).
 `)
 await session.promiseQuery(`
-    apply(succ, [1], Value).
+    A = succ(1).
+    123 = succ(B).
 `)
 for await (const answer of session.promiseAnswers()) {
     console.log(session.format_answer(answer))
